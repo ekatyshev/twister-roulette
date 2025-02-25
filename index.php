a6f2d64a8ee4e4d0631a9e8f5d236194404d1faa
Evgeny Katyshev <eugkat@gmail.com>
2025-02-25 21:24:53 +0400

    Remove Adsense code

--- index.php
+++ index.php
@@ -174,21 +174,5 @@ document.body.appendChild(el);
 </div>
 
 </div>
-<!--
-<aside id="ad_wrapper">
-<div id="ad">
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-1005301514822444"
-     data-ad-slot="6038494857"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-</div>
-</aside>
--->
 </body>
 </html>
