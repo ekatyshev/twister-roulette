e8a02b790291568ca0af30215b5c75c9884e902f
Evgeny Katyshev <eugkat@gmail.com>
2025-02-27 23:50:35 +0400

    Add Unami

--- index.php
+++ index.php
@@ -48,6 +48,8 @@
   gtag('config', 'G-55N0Y2MHR5');
 </script>
 
+<script defer src="https://cloud.umami.is/script.js" data-website-id="17f2105f-8c43-49f2-8d7d-14bd77546619"></script>
+
 <!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(26947902, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/26947902" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
 
 <script data-ad-client="ca-pub-1005301514822444" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
