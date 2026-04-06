f8359951e3ab2798d93f1ac6256fa0d3b48a088d
Evgeny Katyshev <ekatyshev@proton.me>
2026-04-06 13:27:06 +0400

    Comment out base href in index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <head>
 
-<base href="//twister-roulette.com/" />
+<!--<base href="//twister-roulette.com/" />-->
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="robots" content="index, follow" />
